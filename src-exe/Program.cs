4270e9c68bc120ca917695da0ad4819d843fc8db
MizuYaYa <106097784+MizuYaYa@users.noreply.github.com>
2024-04-27 22:20:14 +0900

    feat: exeを使用するためにC#コードを追加

---
+++ src-exe/Program.cs
@@ -0,0 +1,8 @@
+﻿using System.Diagnostics;
+
+var app = new ProcessStartInfo
+{
+    FileName = "powershell",
+    Arguments = @"/c start-process powershell -ArgumentList "".\node-v20.12.2-win-x64\node.exe"","".\src\app.js""",
+};
+Process.Start(app);
